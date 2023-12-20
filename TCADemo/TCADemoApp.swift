9bcfc09341a78441ac3967d97b2143dba69b3591
Dan Tran <dan.tt@amanotes.com>
2023-12-20 22:17:29 +0700

    Update TCADemoApp.swift

--- TCADemo/TCADemoApp.swift
+++ TCADemo/TCADemoApp.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 import ComposableArchitecture
 import Products
+import Carts
 
 @main
 struct TCADemoApp: App {
     var body: some Scene {
         WindowGroup {
+//            CartListView(
+//                store: Store(
+//                    initialState: CartListDomain.State(
+//                        carts: IdentifiedArrayOf(
+//                            uniqueElements: Cart.sample.map {
+//                                CartDomain.State(cart: $0)
+//                            }
+//                        )
+//                    ), reducer: {
+//                        CartListDomain()
+//                    }
+//                )
+//            )
             ProductListView(
                 store: Store(
                     initialState: ProductListDomain.State(),
